da3a70c539f659401336ad301f1d7f15d4049da5
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-06-08 05:45:17 +0530

    Add navbar to the codebase

---
+++ src/app/list/[slug]/page.tsx
@@ -0,0 +1,5 @@
+export default function SingleListPage() {
+    return (
+        <div>SingleListPage</div>
+    )
+}
---
+++ src/app/list/page.tsx
@@ -0,0 +1,6 @@
+export default function ListPage() {
+    return (
+        <div>
+        List Page</div>
+    )
+}
